c9201d19f1c37324b95cb3b6ff9e718d5b8a5de2
wangyuexiang <yuexiang.wang@hotmail.com>
2015-10-22 18:00:58 +0200

    Modif
    rm header
    modif label

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,43 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 The Qt Company Ltd.
-** Contact: http://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** You may use this file under the terms of the BSD license as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #include "mainwindow.h"
 
 #include <QAction>
@@ -50,14 +10,14 @@ MainWindow::MainWindow()
     addressWidget = new AddressWidget;
     setCentralWidget(addressWidget);
     createMenus();
-    setWindowTitle(tr("Address Book"));
+    setWindowTitle(tr("Warehouse Terminal"));
 }
 //! [0]
 
 //! [1a]
 void MainWindow::createMenus()
 {
-    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu = menuBar()->addMenu(tr("&Terminal"));
 
     openAct = new QAction(tr("&Open..."), this);
     fileMenu->addAction(openAct);
@@ -74,27 +34,40 @@ void MainWindow::createMenus()
     fileMenu->addAction(exitAct);
     connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
 
-    toolMenu = menuBar()->addMenu(tr("&Tools"));
+    toolMenu = menuBar()->addMenu(tr("&Orders"));
 
-    addAct = new QAction(tr("&Add Entry..."), this);
+    addAct = new QAction(tr("&Add Order..."), this);
     toolMenu->addAction(addAct);
     connect(addAct, SIGNAL(triggered()), addressWidget, SLOT(addEntry()));
 
 //! [1b]
-    editAct = new QAction(tr("&Edit Entry..."), this);
+    editAct = new QAction(tr("&Edit Order..."), this);
     editAct->setEnabled(false);
     toolMenu->addAction(editAct);
     connect(editAct, SIGNAL(triggered()), addressWidget, SLOT(editEntry()));
 
     toolMenu->addSeparator();
 
-    removeAct = new QAction(tr("&Remove Entry"), this);
+    removeAct = new QAction(tr("&Cancel Order"), this);
     removeAct->setEnabled(false);
     toolMenu->addAction(removeAct);
     connect(removeAct, SIGNAL(triggered()), addressWidget, SLOT(removeEntry()));
 
     connect(addressWidget, SIGNAL(selectionChanged(QItemSelection)),
         this, SLOT(updateActions(QItemSelection)));
+				
+		
+		// productMenu = menuBar()->addMenu(tr("&Product"));
+
+    // addProduct = new QAction(tr("&Add Product..."), this);
+    // productMenu->addAction(addProduct);
+    // connect(addProduct, SIGNAL(triggered()), addressWidget, SLOT(addEntry()));
+
+    // editProduct = new QAction(tr("&Edit Product..."), this);
+    // editProduct->setEnabled(false);
+    // productMenu->addAction(editProduct);
+    // connect(editProduct, SIGNAL(triggered()), addressWidget, SLOT(editEntry()));
+
 }
 //! [1b]
 
